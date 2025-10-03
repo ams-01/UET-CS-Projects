96c46eb5fb688fe5977266c8207ed74618cb0ee7
ams-01 <smamq786@gmail.com>
2025-10-03 15:02:16 +0500

    Add files via upload

---
+++ Assignment1/menu.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main(){
+
+    cout<<"Menu:"<<endl;
+    cout<<"              "<<endl;
+    cout<<"1- Add Student"<<endl;
+    cout<<"2- Show Student"<<endl;
+    cout<<"3- Remove Student"<<endl;
+    cout<<"4- Show All Student"<<endl;
+    cout<<"   Choose any option ---> "<<endl;
+}
